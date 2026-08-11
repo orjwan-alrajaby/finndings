2dc716ed47ac0368e6eb0c561fb57e163a5ce8f5
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2026-08-11 20:16:35 +0300

    remove console.log and replace 'finnLensInjected' with 'finLensProcessed'

--- browser-extension/entrypoints/content/injectors/inject-pin-button/injectPinBtnIntoDetailsPage.ts
+++ browser-extension/entrypoints/content/injectors/inject-pin-button/injectPinBtnIntoDetailsPage.ts
@@ -5,17 +5,15 @@ export function injectPinBtnIntoDetailsPage() {
     'div[data-appid="product-details"]'
   );
 
-  if (!root || root.dataset.finnLensInjected === "true") return;
+  if (!root || root.dataset.finnLensProcessed === "true") return;
 
   const anchorElements = Array.from<HTMLDivElement>(
     root.querySelectorAll('[id^="product-"]')
   ).filter((el) => /^product-\d+$/.test(el.id)); // match to elements of id values of e.g product-32710
-  console.log("anchorElements", anchorElements);
-
   const buttons = anchorElements.map(element => injectPinCarButtonIntoNode(element))
 
   if (buttons.length > 0) {
-    anchorElements.forEach(element => element.dataset.finnLensInjected = "true");
+    anchorElements.forEach(element => element.dataset.finnLensProcessed = "true");
     const compareBtns = Array.from<HTMLButtonElement>(root.querySelectorAll('button[aria-label="Vergleichen"]'));
     compareBtns.forEach(btn => btn.classList.add("right-unset", "left-4"));
   }
